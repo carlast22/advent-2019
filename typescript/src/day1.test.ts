95d47246c6f053e55b5f339fefc0d12c85ed5c07
Carla Suarez <carlast22@gmail.com>
2019-12-05 00:14:47 -0500

    Fixes recursion and mutation

--- typescript/src/day1.test.ts
+++ typescript/src/day1.test.ts
@@ -1,32 +1,31 @@
 import { bestFuel, calculateTotalFuel } from './day1'
 
 test('if mass is 12 fuel should be 2', () => {
-    const mass = 12
-    const result = bestFuel(mass)
-    expect(result).toBe(2)
+  const mass = 12
+  const result = bestFuel(mass)
+  expect(result).toBe(2)
 })
 
 test('if mass is 14 fuel should be 2', () => {
-    const mass = 14
-    const result = bestFuel(mass)
-    expect(result).toBe(2)
+  const mass = 14
+  const result = bestFuel(mass)
+  expect(result).toBe(2)
 })
 
 test('if mass is 1969 fuel should be 966', () => {
-    const mass = 1969
-    const result = bestFuel(mass)
-    expect(result).toBe(966)
+  const mass = 1969
+  const result = bestFuel(mass)
+  expect(result).toBe(966)
 })
 
 test('if mass is 100756 fuel should be 50346', () => {
-    const mass = 100756
-    const result = bestFuel(mass)
-    expect(result).toBe(50346)
+  const mass = 100756
+  const result = bestFuel(mass)
+  expect(result).toBe(50346)
 })
 
-xtest('return the total of fuels given and array of masses', () => {
-    const data = '12, 14, 1969, 100756'
-    const result = calculateTotalFuel(data)
-    expect(result).toBe(51316)
+test('return the total of fuels given and array of masses', () => {
+  const data = '12\n14\n1969\n100756'
+  const result = calculateTotalFuel(data)
+  expect(result).toBe(51316)
 })
-
--- typescript/src/day1.ts
+++ typescript/src/day1.ts
@@ -1,28 +1,36 @@
-import fs from 'fs'
+import * as fs from 'fs'
 
 const currentInput = fs.readFileSync('typescript/inputs/day1.txt', 'utf8')
 
+function add(accumulator: number, n: string): number {
+  const number = +n
+  return accumulator + bestFuel(number)
+}
+
 function fuel(number: number) {
-    return Math.floor(number / 3) - 2
+  return Math.max(0, Math.floor(number / 3) - 2)
 }
 
 export function bestFuel(mass: number): number {
-    if (mass <= 6) return 0
-    let result: number = fuel(mass)
-    return result + bestFuel(result) 
+  let finalSum = 0
+  let calculatedFuel = 0
+  let currentMass = mass
+
+  do {
+    calculatedFuel = fuel(currentMass)
+    finalSum += calculatedFuel
+    currentMass = calculatedFuel
+  } while (calculatedFuel > 0)
+  return finalSum
 }
 
 export function calculateTotalFuel(input: string): number {
-    let total: number = 0;
-    const data: string[] = input.split('\n');
+  let total = 0
+  const data = input.split('\n')
 
-    data.map(n => {
-        const number = +n
-        total += bestFuel(number);
-    })
-
-    return total;
+  total = data.reduce(add, 0)
+  return total
 }
 
-const result = calculateTotalFuel(currentInput);
+const result = calculateTotalFuel(currentInput)
 console.log(`This is the total fuel required: ${result}`)
